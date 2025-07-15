1a2b34ab5a56b62efcab3eb4990bb6da029d5b33
Youssef Meskini <youss.meskini@gmail.com>
2025-07-15 12:06:49 +0100

    add an example for mcp connection

---
+++ src/lib/mcp.ts
@@ -0,0 +1,29 @@
+import { google } from "@ai-sdk/google";
+import { experimental_createMCPClient as createMCPClient, streamText } from "ai";
+
+// this is an example of how to create a client for the MCP (Multi-Channel Protocol)
+// and how to use it with ai SDK
+
+const mcpClient = await createMCPClient({
+  transport: {
+    type: "sse",
+    url: "https://my-server.com/sse",
+
+    // optional: configure HTTP headers, e.g. for authentication
+    headers: {
+      Authorization: "Bearer my-api-key",
+    },
+  },
+});
+
+
+const tools = await mcpClient.tools();
+
+streamText({
+  model: google("gemini-2.0-flash-001"),
+  tools,
+  prompt: "What is the weather in Brooklyn, New York?",
+  onFinish: async () => {
+    await mcpClient.close();
+  },
+});
