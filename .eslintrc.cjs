a8e156bcd3e79a701fd9c0130665786dc3e00fdf
Youssef Meskini <youss.meskini@gmail.com>
2026-04-01 13:30:37 +0100

    chore: setup react query

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ const config = {
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:@tanstack/query/recommended",
   ],
   rules: {
     "@typescript-eslint/no-namespace": ["error", { allowDeclarations: true }],
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import Providers from "./providers";
 
 export const metadata: Metadata = {
   title: "AI App Example",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
